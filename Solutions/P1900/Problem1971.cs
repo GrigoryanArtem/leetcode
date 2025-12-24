dfc532a02fdcc1ea401b65d168b001b47973ecbf
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-12-24 14:41:12 +0200

    1971

---
+++ Solutions/P1900/Problem1971.cs
@@ -0,0 +1,45 @@
+﻿namespace Solutions.P1900;
+
+public class Problem1971
+{
+    public bool ValidPath(int n, int[][] edges, int source, int destination)
+    {
+        var uf = new UnionFind(n, source, destination);
+        for (int i = 0; i < edges.Length && !uf.PathExist; i++)
+            uf.Union(edges[i][0], edges[i][1]);
+
+        return uf.PathExist;
+    }
+
+    private class UnionFind(int n, int source, int destination)
+    {
+        private readonly int[] _parent = [.. Enumerable.Range(0, n)];
+
+        private int _sourceRoot = source;
+        private int _destinationRoot = destination;
+
+        public bool PathExist => _sourceRoot == _destinationRoot;
+
+        public int Find(int x)
+            => _parent[x] != x
+                ? _parent[x] = Find(_parent[x])
+                : x;
+
+        public void Union(int x, int y)
+        {
+            x = Find(x);
+            y = Find(y);
+
+            if (x == y)
+                return;
+
+            _parent[y] = x;
+
+            if (y == _sourceRoot)
+                _sourceRoot = x;
+
+            if (y == _destinationRoot)
+                _destinationRoot = x;
+        }
+    }
+}
---
+++ Tests/P1900/Problem1971Tests.cs
@@ -0,0 +1,31 @@
+﻿using Solutions.P1900;
+
+namespace Tests.P1900;
+
+[Trait("Problem", "1900+")]
+public class Problem1971Tests
+{
+    [Theory(DisplayName = "1971")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void FindJudge_Examples((int n, int[][] edges, int source, int destination, bool expected) testCase)
+    {
+        var problem = new Problem1971();
+
+        var answer = problem.ValidPath
+        (
+            testCase.n, 
+            testCase.edges, 
+            testCase.source, 
+            testCase.destination
+        );
+
+        answer.Should().Be(testCase.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int n, int[][] edges, int source, int destination, bool expected)> Examples()
+    {
+        yield return (3, [[0, 1], [1, 2], [2, 0]], 0, 2, true);
+        yield return (6, [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]], 0, 5, false);
+    }
+}
