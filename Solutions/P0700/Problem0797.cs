c193ed5f070f45db7bfa26991a0e98ea4d5028ee
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-12-24 03:33:29 +0200

    797

---
+++ Solutions/P0700/Problem0797.cs
@@ -0,0 +1,56 @@
+﻿namespace Solutions.P0700;
+
+public class Problem0797
+{
+    public IList<IList<int>> AllPathsSourceTarget(int[][] graph)
+        => DFS(graph, 0, graph.Length - 1);
+
+    private static List<IList<int>> DFS(int[][] graph, int start, int target)
+    {
+        Span<int> path = stackalloc int[graph.Length];
+        var pidx = 0;
+
+        Span<int> stack = stackalloc int[graph.Length];
+        var sidx = 0;
+
+        var result = new List<IList<int>>();
+
+        stack[sidx++] = 0;
+        path[pidx++] = 0;
+
+        while (sidx > 0)
+        {
+            var (node, childIndex) = FromNc(stack[--sidx]);
+
+            if (node == target)
+            {
+                var copy = new int[pidx];
+                for (int i = 0; i < pidx; i++)
+                    copy[i] = path[i];
+
+                result.Add(copy);
+                pidx--;
+
+                continue;
+            }
+
+            if (childIndex < graph[node].Length)
+            {
+                stack[sidx++] = ToNC(node, childIndex + 1);
+                path[pidx++] = stack[sidx++] = graph[node][childIndex];                
+            }
+            else
+            {
+                pidx--;
+            }
+        }
+
+        return result;
+    }
+
+    private static int ToNC(int node, int child)
+        => (child << 8) | node;
+
+    private static (int node, int child) FromNc(int nc)
+        => (nc & 0xFF, nc >> 8);    
+}
---
+++ Tests/P0700/Problem0797Tests.cs
@@ -0,0 +1,25 @@
+﻿using Solutions.P0700;
+
+namespace Tests.P0700;
+
+[Trait("Problem", "700+")]
+public class Problem0797Tests
+{
+    [Theory(DisplayName = "797")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void FindJudge_Examples((int[][] graph, int[][] expected) @case)
+    {
+        var problem = new Problem0797();
+
+        var answer = problem.AllPathsSourceTarget(@case.graph);
+
+        answer.Should().BeEquivalentTo(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[][] graph, int[][] expected)> Examples()
+    {
+        yield return ([[1, 2], [3], [3], []], [[0, 1, 3], [0, 2, 3]]);
+        yield return ([[4, 3, 1], [3, 2, 4], [3], [4], []], [[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]]);
+    }
+}
