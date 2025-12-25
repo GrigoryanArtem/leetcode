05c6f1d37f1fb46d48934ea8239438c85a61677d
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-12-25 23:55:47 +0200

    1557

---
+++ Solutions/P1500/Problem1557.cs
@@ -0,0 +1,18 @@
+﻿namespace Solutions.P1500;
+
+public class Problem1557
+{
+    public IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges)
+    {
+        Span<byte> hasParent = stackalloc byte[n];
+        for (int i = 0; i < edges.Count; i++)
+            hasParent[edges[i][1]] = 1;
+
+        var result = new List<int>();
+        for (int i = 0; i < hasParent.Length; i++)
+            if (hasParent[i] == 0)
+                result.Add(i);
+
+        return result;
+    }
+}
---
+++ Tests/P1500/Problem1557Tests.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.P1500;
+
+namespace Tests.P1500;
+
+[Trait("Problem", "1500+")]
+public class Problem1557Tests
+{
+    [Theory(DisplayName = "1557")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void FindJudge_Examples((int n, IList<IList<int>> edges, IList<int> expected) testCase)
+    {
+        var problem = new Problem1557();
+
+        var answer = problem.FindSmallestSetOfVertices
+        (
+            testCase.n,
+            testCase.edges
+        );
+
+        answer.Should().BeEquivalentTo(testCase.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int n, IList<IList<int>> edges, IList<int> expected)> Examples()
+    {
+        yield return (6, [[0, 1], [0, 2], [2, 5], [3, 4], [4, 2]], [0, 3]);
+        yield return (5, [[0, 1], [2, 1], [3, 1], [1, 4], [2, 4]], [0, 2, 3]);
+    }
+}
