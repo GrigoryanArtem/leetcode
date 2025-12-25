1cb17051ec17a4a999c7ed70a88a005246c7bc4a
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-12-25 03:25:13 +0200

    1061

---
+++ Solutions/P1000/Problem1061.cs
@@ -0,0 +1,52 @@
+﻿namespace Solutions.P1000;
+
+public class Problem1061
+{
+    public string SmallestEquivalentString(string s1, string s2, string baseStr)
+    {
+        var uf = new UnionFind();
+
+        for (int i = 0; i < s1.Length; i++)
+            uf.Union(s1[i] - 'a', s2[i] - 'a');
+
+        var res = baseStr.ToCharArray();
+        for (int i = 0; i < res.Length; i++)
+            res[i] = (char)(uf.Find(res[i] - 'a') + 'a');
+
+        return new string(res);
+    }
+
+    private class UnionFind
+    {
+        private readonly int[] _parent = new int['z' - 'a' + 1];
+
+        public UnionFind()
+        {
+            for(int i = 0; i < _parent.Length; i++)
+                _parent[i] = i;
+        }
+
+        public int Find(int x)
+            => _parent[x] != x
+                ? _parent[x] = Find(_parent[x])
+                : x;
+
+        public void Union(int x, int y)
+        {
+            x = Find(x);
+            y = Find(y);
+
+            if (x == y)
+                return;
+
+            if (y > x)
+            {
+                _parent[y] = x;
+            }
+            else
+            {
+                _parent[x] = y;
+            }
+        }
+    }
+}
---
+++ Tests/P1000/Problem1061Tests.cs
@@ -0,0 +1,31 @@
+﻿using Solutions.P1000;
+
+namespace Tests.P1000;
+
+[Trait("Problem", "1000+")]
+public class Problem1061Tests
+{
+    [Theory(DisplayName = "1061")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void FindJudge_Examples((string s1, string s2, string baseStr, string expected) testCase)
+    {
+        var problem = new Problem1061();
+
+        var answer = problem.SmallestEquivalentString
+        (
+            testCase.s1,
+            testCase.s2,
+            testCase.baseStr
+        );
+
+        answer.Should().Be(testCase.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(string s1, string s2, string baseStr, string expected)> Examples()
+    {
+        yield return ("parker", "morris", "parser", "makkek");
+        yield return ("hello", "world", "hold", "hdld");
+        yield return ("leetcode", "programs", "sourcecode", "aauaaaaada");
+    }
+}
