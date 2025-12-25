a649a549b2b016b6a302fe5d1048474b3217e5e2
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-12-26 01:09:57 +0200

    3512

---
+++ Solutions/P3500/Problem3512.cs
@@ -0,0 +1,13 @@
+﻿namespace Solutions.P3500;
+
+public class Problem3512
+{
+    public int MinOperations(int[] nums, int k)
+    {
+        var sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+            sum += nums[i];
+
+        return sum % k;
+    }
+}
---
+++ Tests/P3500/Problem3512Tests.cs
@@ -0,0 +1,31 @@
+﻿using Solutions.P3500;
+
+namespace Tests.P3500;
+
+[Trait("Problem", "3500+")]
+public class Problem3512Tests
+{
+
+    [Theory(DisplayName = "3512")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void FindJudge_Examples((int[] nums, int k, int expected) testCase)
+    {
+        var problem = new Problem3512();
+
+        var answer = problem.MinOperations
+        (
+            testCase.nums,
+            testCase.k
+        );
+
+        answer.Should().Be(testCase.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[] nums, int k, int expected)> Examples()
+    {
+        yield return ([3, 9, 7], 5, 4);
+        yield return ([4, 1, 3], 4, 0);
+        yield return ([3, 2], 6, 5);
+    }
+}
