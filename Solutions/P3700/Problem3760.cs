10d06b1214ea14c7556bcbaafde2c4394a7536b6
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-12-27 21:46:10 +0200

    3760

---
+++ Solutions/P3700/Problem3760.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace Solutions.P3700;
+
+public class Problem3760
+{
+    public int MaxDistinct(string s)
+    {
+        var set = 0U;
+        for(int i = 0; i < s.Length; i++)
+            set |= 1U << (s[i] - 'a');
+
+        return BitOperations.PopCount(set);
+    }
+}
---
+++ Tests/P3700/Problem3760Tests.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.P3700;
+
+namespace Tests.P3700;
+
+[Trait("Problem", "3700+")]
+public class Problem3760Tests
+{
+    [Theory(DisplayName = "3760")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void FindJudge_Examples((string s, int n) testCase)
+    {
+        var problem = new Problem3760();
+
+        var answer = problem.MaxDistinct
+        (
+            testCase.s
+        );
+
+        answer.Should().Be(testCase.n);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(string s, int n)> Examples()
+    {
+        yield return ("abab", 2);
+        yield return ("abcd", 4);
+        yield return ("aaaa", 1);
+    }
+}
