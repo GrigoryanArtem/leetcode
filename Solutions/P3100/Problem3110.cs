b0be5848ac908506472eca937dce532c94e2dea5
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-12-30 16:40:57 +0200

    3110

---
+++ Solutions/P3100/Problem3110.cs
@@ -0,0 +1,13 @@
+﻿namespace Solutions.P3100;
+public class Problem3110
+{
+    public int ScoreOfString(string s)
+    {
+        var ans = 0;
+
+        for (int i = 1; i < s.Length; i++)
+            ans += Math.Abs(s[i - 1] - s[i]);
+
+        return ans;
+    }
+}
---
+++ Tests/P3100/Problem3110Tests.cs
@@ -0,0 +1,26 @@
+﻿namespace Tests.P3100;
+
+[Trait("Problem", "3100+")]
+public class Problem3110Tests
+{
+    [Theory(DisplayName = "3512")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void FindJudge_Examples((string s, int expected) testCase)
+    {
+        var problem = new Problem3110();
+
+        var answer = problem.MinOperations
+        (
+            testCase.s
+        );
+
+        answer.Should().Be(testCase.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(string s, int expected)> Examples()
+    {
+        yield return ("hello", 13);
+        yield return ("zaz", 50);
+    }
+}
