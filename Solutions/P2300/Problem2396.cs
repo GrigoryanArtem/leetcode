ecd4cd5b2f72d67d49ca4e2e06dce15e17b520bd
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-12-31 19:39:31 +0200

    2369

---
+++ Solutions/P2300/Problem2396.cs
@@ -0,0 +1,40 @@
+﻿namespace Solutions.P2300;
+
+public class Problem2396
+{
+    public bool IsStrictlyPalindromic(int n)
+    {
+        Span<int> buffer = new int[0x20];
+        var isPalindromic = true;
+
+        for(int b = 2; isPalindromic & b < n; b++)
+        {
+            var s = ToBase(n, b, buffer);
+            isPalindromic &= IsPalindromic(s);
+        }
+
+        return isPalindromic;
+    }
+
+    private static bool IsPalindromic(Span<int> arr)
+    {
+        var isPalindromic = true;
+        for (int left = 0, right = arr.Length - 1; isPalindromic & left <= right; left++, right--)
+            isPalindromic &= arr[left] == arr[right];
+
+        return isPalindromic;
+    }
+
+    private static Span<int> ToBase(int n, int b, Span<int> buffer)
+    {
+        int count = 0;  
+
+        while(n > 0)
+        {
+            buffer[count++] = n % b; 
+            n /= b;
+        }
+            
+        return buffer[.. count];
+    }
+}
---
+++ Tests/P2300/Problem2396Tests.cs
@@ -0,0 +1,28 @@
+﻿using Solutions.P2300;
+
+namespace Tests.P2300;
+
+[Trait("Problem", "2300+")]
+public class Problem2396Tests
+{
+    [Theory(DisplayName = "2396")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void FindJudge_Examples((int n, bool expected) testCase)
+    {
+        var problem = new Problem2396();
+
+        var answer = problem.IsStrictlyPalindromic
+        (
+            testCase.n
+        );
+
+        answer.Should().Be(testCase.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int n, bool expected)> Examples()
+    {
+        yield return (9, false);
+        yield return (4, false);        
+    }
+}
