ceb561109108d0f0cfe198a63bf6c4e938cbd3a0
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-01 22:18:46 +0200

    2574

---
+++ Solutions/P2500/Problem2574.cs
@@ -0,0 +1,20 @@
+﻿namespace Solutions.P2500;
+
+public class Problem2574
+{
+    public int[] LeftRightDifference(int[] nums)
+    {
+        var result = new int[nums.Length];
+        for (int i = nums.Length - 1; i > 0; i--)
+            result[i - 1] = result[i] + nums[i];
+
+        var acc = 0;
+        for(int i = 0; i < nums.Length; i++)
+        {
+            result[i] = Math.Abs(acc - result[i]);
+            acc += nums[i];
+        }
+
+        return result;
+    }
+}
---
+++ Tests/P2500/Problem2574Tests.cs
@@ -0,0 +1,25 @@
+﻿using Solutions.P2500;
+
+namespace Tests.P2500;
+
+[Trait("Problem", "2500+")]
+public class Problem2574Tests
+{
+    [Theory(DisplayName = "2574")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void ExamplesTest((int[] nums, int[] expected) @case)
+    {
+        var problem = new Problem2574();
+
+        var answer = problem.LeftRightDifference(@case.nums);
+
+        answer.Should().BeEquivalentTo(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[] nums, int[] expected)> Examples()
+    {
+        yield return ([10, 4, 8, 3], [15, 1, 11, 22]);
+        yield return ([1], [0]);
+    }
+}
