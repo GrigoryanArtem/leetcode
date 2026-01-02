9ccd456a16d2f2f0daa7c77987122894628d3ac3
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-02 20:54:47 +0200

    2391

---
+++ Solutions/P2300/Problem2391.cs
@@ -0,0 +1,37 @@
+﻿namespace Solutions.P2300;
+
+public class Problem2391
+{
+    public int GarbageCollection(string[] garbage, int[] travel)
+    {
+        var time = 0;
+        var found = 0U;
+
+        Span<char> names = ['M', 'P', 'G'];
+        Span<int> idx = stackalloc int[names.Length];
+
+        Span<int> travelTime = stackalloc int[travel.Length + 1];
+        for (int i = 0; i < travelTime.Length - 1; i++)
+            travelTime[i + 1] = travel[i] + travelTime[i];
+
+        for (int i = garbage.Length - 1; found != 0b111 && i >= 0; i--)
+        {
+            for (int k = 0; k < idx.Length; k++)
+            {
+                if(((found >> k) & 1U) == 0U && garbage[i].Contains(names[k]))
+                {
+                    found |= (1U << k);
+                    idx[k] = i;
+                }
+            }
+        }
+
+        for (int i = 0; i < garbage.Length; i++)
+            time += garbage[i].Length;
+
+        for (int i = 0; i < idx.Length; i++)
+            time += travelTime[idx[i]];
+
+        return time;
+    }
+}
---
+++ Tests/P2300/Problem2391Tests.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.P2300;
+
+namespace Tests.P2300;
+
+[Trait("Problem", "2300+")]
+public class Problem2391Tests
+{
+    [Theory(DisplayName = "2391")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void ExamplesTest((string[] grabage, int[] travel, int expected) @case)
+    {
+        var problem = new Problem2391();
+
+        var answer = problem.GarbageCollection
+        (
+            @case.grabage,
+            @case.travel
+        );
+
+        answer.Should().Be(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(string[] grabage, int[] travel, int expected)> Examples()
+    {
+        yield return (["G", "P", "GP", "GG"], [2, 4, 3], 21);
+        yield return (["MMM", "PGM", "GP"], [3, 10], 37);
+    }
+}
