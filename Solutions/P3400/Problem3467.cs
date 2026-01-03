12541e7a2d29c9f014cff3a810b8cdb9963dd85b
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-03 23:44:52 +0200

    3467

---
+++ Solutions/P3400/Problem3467.cs
@@ -0,0 +1,16 @@
+﻿namespace Solutions.P3400;
+
+public class Problem3467
+{
+    public int[] TransformArray(int[] nums)
+    {
+        var result = new int[nums.Length];
+        var right = nums.Length;
+
+        for (int i = 0; i < nums.Length; i++)
+            if (nums[i] % 2 != 0)
+                result[--right] = 1;
+
+        return result;
+    }
+}
---
+++ Tests/P3400/Problem3467Tests.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.P3400;
+
+namespace Tests.P3400;
+
+[Trait("Problem", "3400+")]
+public class Problem3467Tests
+{
+
+    [Theory(DisplayName = "3467")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void ExamplesTest((int[] nums, int[] expected) @case)
+    {
+        var problem = new Problem3467();
+
+        var answer = problem.TransformArray
+        (
+            @case.nums
+        );
+
+        answer.Should().BeEquivalentTo(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[] nums, int[] expected)> Examples()
+    {
+        yield return ([4, 3, 2, 1], [0, 0, 1, 1]);
+        yield return ([1, 5, 1, 4, 2], [0, 0, 1, 1, 1]);
+    }
+}
