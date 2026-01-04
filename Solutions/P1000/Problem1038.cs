a2db4d7831ef851aa175b86d07ad99ad84c04f3f
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-04 20:04:03 +0200

    1038

---
+++ Solutions/P1000/Problem1038.cs
@@ -0,0 +1,29 @@
+﻿namespace Solutions.P1000;
+
+public class Problem1038
+{
+    public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        public int val = val;
+
+        public TreeNode? left = left;
+        public TreeNode? right = right;
+    }
+
+    private int _currentSum = 0;
+
+    public TreeNode? BstToGst(TreeNode? root)
+    {
+        if (root is null)
+            return null;
+
+        BstToGst(root.right);
+
+        _currentSum += root.val;
+        root.val = _currentSum;
+
+        BstToGst(root.left);
+
+        return root;
+    }
+}
