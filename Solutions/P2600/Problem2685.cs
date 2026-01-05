e0698ad2776899363d59bc401692e71729b94e6a
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-05 21:25:32 +0200

    2685

---
+++ Solutions/P2600/Problem2685.cs
@@ -0,0 +1,79 @@
+﻿namespace Solutions.P2600;
+
+public class Problem2685
+{
+    public int CountCompleteComponents(int n, int[][] edges)
+    {
+        var uf = new UnionFind(n);
+
+        foreach (var edge in edges)
+            uf.Union(edge[0], edge[1]);
+
+        var count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i != uf.Parent[i])
+                continue;
+
+            if (EdgesCount(uf.Rank(i)) == uf.Connections(i))
+                count++;
+        }
+
+        return count;
+    }
+
+    public static int EdgesCount(int rank)
+        => rank * (rank - 1) / 2;
+
+    private class UnionFind
+    {
+        private readonly int[] _ranks;
+        private readonly int[] _connections;
+
+        public int[] Parent { get; }
+
+        public UnionFind(int n)
+        {
+            Parent = new int[n];
+
+            _ranks = new int[n];
+            _connections = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Parent[i] = i;
+                _ranks[i] = 1;
+            }
+        }
+
+        public int Connections(int x)
+            => _connections[x];
+
+        public int Rank(int x)
+            => _ranks[x];
+
+        public int Find(int x)
+            => Parent[x] != x
+                ? Parent[x] = Find(Parent[x])
+                : x;
+
+        public void Union(int x, int y)
+        {
+            x = Find(x);
+            y = Find(y);
+
+            if (x == y)
+            {
+                _connections[x]++;
+                return;
+            }
+
+            if (_ranks[x] < _ranks[y])
+                (x, y) = (y, x);
+
+            Parent[y] = x;
+            _ranks[x] += _ranks[y];
+            _connections[x] += _connections[y] + 1;
+        }
+    }
+}
---
+++ Tests/P2600/Problem2685Tests.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.P2600;
+
+namespace Tests.P2600;
+
+[Trait("Problem", "2600+")]
+public class Problem2685Tests
+{
+    [Theory(DisplayName = "2685")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void ExamplesTest((int n, int[][] edges, int expected) @case)
+    {
+        var problem = new Problem2685();
+
+        var answer = problem.CountCompleteComponents
+        (
+            @case.n,
+            @case.edges
+        );
+
+        answer.Should().Be(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int n, int[][] edges, int expected)> Examples()
+    {
+        yield return (6, [[0, 1], [0, 2], [1, 2], [3, 4]], 3);
+        yield return (6, [[0, 1], [0, 2], [1, 2], [3, 4], [3, 5]], 1);
+    }
+}
