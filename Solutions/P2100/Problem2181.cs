52e9f75ad761a76560368ea41f3c93087a86aee3
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-06 23:57:17 +0200

    2181

---
+++ Solutions/P2100/Problem2181.cs
@@ -0,0 +1,33 @@
+﻿namespace Solutions.P2100;
+
+public class Problem2181
+{
+    public class ListNode(int val = 0, ListNode? next = null)
+    {
+        public int val = val;
+        public ListNode? next = next;
+    }
+
+    public ListNode MergeNodes(ListNode head)
+    {
+        var prev = head;
+        var currentSum = 0;
+
+        for (var node = head.next; node is not null; node = node.next)
+        {
+            if (node.val == 0)
+            {
+                prev.next!.val = currentSum;
+                prev = prev.next;
+                currentSum = 0;
+            }
+            else
+            {
+                currentSum += node.val;
+            }
+        }
+
+        prev.next = null;
+        return head.next!;
+    }
+}
---
+++ Tests/P2100/Problem2181Tests.cs
@@ -0,0 +1,54 @@
+﻿using Solutions.P2100;
+using static Solutions.P2100.Problem2181;
+
+namespace Tests.P2100;
+
+[Trait("Problem", "2100+")]
+public class Problem2181Tests
+{
+    [Theory(DisplayName = "2181")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void FindJudge_Examples((ListNode head, ListNode expected) testCase)
+    {
+        var problem = new Problem2181();
+
+        var answer = problem.MergeNodes
+        (
+            testCase.head
+        );
+
+        var expected = testCase.expected;
+        while (expected is not null)
+        {
+            answer.Should().NotBeNull();
+            expected.val.Should().Be(answer.val);
+
+            answer = answer.next;
+            expected = expected.next;
+        }
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(ListNode? head, ListNode? expected)> Examples()
+    {
+        yield return (A2LN([0, 3, 1, 0, 4, 5, 2, 0]), A2LN([4, 11]));
+        yield return (A2LN([0, 1, 0, 3, 0, 2, 2, 0]), A2LN([1, 3, 4]));
+    }
+
+    private static ListNode? A2LN(int[] arr)
+    {
+        if (arr.Length == 0)
+            return null;
+
+        var head = new ListNode(arr[0]);
+        var curr = head;
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            curr.next = new ListNode(arr[i]);
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
