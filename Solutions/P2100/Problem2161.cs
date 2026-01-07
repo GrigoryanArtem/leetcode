5b92f5f6438c0bb3531691fc0631b816f0792fa3
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-07 20:50:00 +0200

    2161

---
+++ Solutions/P2100/Problem2161.cs
@@ -0,0 +1,26 @@
+﻿namespace Solutions.P2100;
+
+public class Problem2161
+{
+    public int[] PivotArray(int[] nums, int pivot)
+    {
+        var result = new int[nums.Length];
+
+        var left = 0;
+        var right = nums.Length - 1;
+
+        for (int i = 0, k = nums.Length - 1; i < nums.Length; i++, k--)
+        {
+            if (nums[i] < pivot)
+                result[left++] = nums[i];
+
+            if (nums[k] > pivot)
+                result[right--] = nums[k];
+        }
+
+        for (int i = left; i <= right; i++)
+            result[i] = pivot;
+
+        return result;
+    }
+}
---
+++ Tests/P2100/Problem2161Tests.cs
@@ -0,0 +1,30 @@
+﻿using Solutions.P2100;
+
+namespace Tests.P2100;
+
+[Trait("Problem", "2100+")]
+public class Problem2161Tests
+{
+    [Theory(DisplayName = "2161")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void TestExamples((int[] nums, int pivot, int[] expected) @case)
+    {
+        var problem = new Problem2161();
+
+        var answer = problem.PivotArray
+        (
+            @case.nums,
+            @case.pivot
+        );
+
+        answer.Should().BeEquivalentTo(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[] nums, int pivot, int[] @case)> Examples()
+    {
+        yield return ([9, 12, 5, 10, 14, 3, 10], 10, [9, 5, 3, 10, 10, 12, 14]);
+        yield return ([-3, 4, 3, 2], 2, [-3, 2, 4, 3]);
+        yield return ([10, 10, 10, 10, 10], 10, [10, 10, 10, 10, 10]);
+    }
+}
