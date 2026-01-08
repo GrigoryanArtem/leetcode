a56f7759954679ed2f89785d4e0f72560c37f6f8
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-08 21:00:26 +0200

    448

---
+++ Solutions/P0400/Problem448.cs
@@ -0,0 +1,21 @@
+﻿namespace Solutions.P0400;
+
+public class Problem448
+{
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int pos = Math.Abs(nums[i]) - 1;
+            if (nums[pos] > 0)
+                nums[pos] = -nums[pos];
+        }
+
+        var result = new List<int>();
+        for (int i = 0; i < nums.Length; i++)
+            if (nums[i] > 0) 
+                result.Add(i + 1);
+
+        return result;
+    }
+}
---
+++ Tests/P0400/Problem448Tests.cs
@@ -0,0 +1,28 @@
+﻿using Solutions.P0400;
+
+namespace Tests.P0400;
+
+[Trait("Problem", "400+")]
+public class Problem448Tests
+{
+    [Theory(DisplayName = "448")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void TestExamples((int[] nums, IList<int> expected) @case)
+    {
+        var problem = new Problem448();
+
+        var answer = problem.FindDisappearedNumbers
+        (
+            @case.nums
+        );
+
+        answer.Should().BeEquivalentTo(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[] nums, IList<int> expected)> Examples()
+    {
+        yield return ([4, 3, 2, 7, 8, 2, 3, 1], [5, 6]);
+        yield return ([1, 1], [2]);
+    }
+}
