863f8f6717f1a441b7fee700635dc0e1124c8ef3
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-10 21:08:13 +0200

    84

---
+++ Solutions/P0000/Problem0084.cs
@@ -0,0 +1,36 @@
+﻿namespace Solutions.P0000;
+
+public class Problem0084
+{
+    public int LargestRectangleArea(int[] heights)
+    {
+        var n = heights.Length;
+        Span<int> stack = stackalloc int[n];
+        var top = -1;
+
+        int maxArea = 0;
+
+        for (int i = 0; i < heights.Length; i++)
+        {
+            while (top >= 0 && heights[stack[top]] >= heights[i])
+            {
+                var val = stack[top--];
+                var width = top >= 0 ? i - stack[top] - 1 : i;
+
+                maxArea = Math.Max(maxArea, heights[val] * width);
+            }
+
+            stack[++top] = i;
+        }
+
+        while (top >= 0)
+        {
+            var val = stack[top--];
+            var width = top >= 0 ? n - stack[top] - 1 : n;
+
+            maxArea = Math.Max(maxArea, heights[val] * width);
+        }
+
+        return maxArea;
+    }
+}
---
+++ Tests/P0000/Problem0084Tests.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.P0000;
+
+namespace Tests.P0000;
+
+
+[Trait("Problem", "0+")]
+public class Problem0084Tests
+{
+    [Theory(DisplayName = "84")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void TestExamples((int[] heights, int expected) @case)
+    {
+        var problem = new Problem0084();
+
+        var answer = problem.LargestRectangleArea
+        (
+            @case.heights
+        );
+
+        answer.Should().Be(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[] heights, int expected)> Examples()
+    {        
+        yield return ([2, 1, 5, 6, 2, 3], 10);
+        yield return ([2, 4], 4);
+    }
+}
