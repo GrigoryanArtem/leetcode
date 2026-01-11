fb4e854528e575780fde0033cf7e0f86772dc89f
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-11 23:58:00 +0200

    1700

---
+++ Solutions/P1700/Problem1700.cs
@@ -0,0 +1,22 @@
+﻿namespace Solutions.P1700;
+
+public class Problem1700
+{
+    public int CountStudents(int[] students, int[] sandwiches)
+    {
+        Span<int> count = stackalloc int[2];
+
+        foreach (int s in students)
+            count[s]++;
+
+        foreach (int s in sandwiches)
+        {
+            if (count[s] == 0)
+                break;
+
+            count[s]--;
+        }
+
+        return count[0] + count[1];
+    }
+}
---
+++ Tests/P1700/Problem1700Tests.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.P1700;
+
+namespace Tests.P1700;
+
+[Trait("Problem", "1700+")]
+public class Problem1700Tests
+{
+    [Theory(DisplayName = "1700")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void TestExamples((int[] students, int[] sandwiches, int expected) @case)
+    {
+        var problem = new Problem1700();
+
+        var answer = problem.CountStudents
+        (
+            @case.students,
+            @case.sandwiches
+        );
+
+        answer.Should().Be(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[] students, int[] sandwiches, int expected)> Examples()
+    {
+        yield return ([1, 1, 0, 0], [0, 1, 0, 1], 0);
+        yield return ([1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1], 3);
+    }
+}
