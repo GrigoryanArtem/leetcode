94e3711654fba0eaa8085d4e7d60556dee9e2c78
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-13 23:34:34 +0200

    1266

---
+++ Solutions/P1200/Problem1266.cs
@@ -0,0 +1,25 @@
+﻿namespace Solutions.P1200;
+
+public class Problem1266
+{
+    public int MinTimeToVisitAllPoints(int[][] points)
+    {
+        var current = points[0];
+        var time = 0;
+
+        for(int i = 1; i < points.Length; i++)
+        {
+            var end = points[i];
+            var dx = Math.Abs(end[0] - current[0]);
+            var dy = Math.Abs(end[1] - current[1]);
+
+            var sqrt = Math.Min(dx, dy);
+
+            time += dx + dy - sqrt;
+
+            current = end;
+        }
+
+        return time;
+    }
+}
---
+++ Tests/P1200/Problem1266Tests.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.P1200;
+
+namespace Tests.P1200;
+
+[Trait("Problem", "1200+")]
+public class Problem1266Tests
+{
+
+    [Theory(DisplayName = "1266")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void TestExamples((int[][] points, int expected) @case)
+    {
+        var problem = new Problem1266();
+
+        var answer = problem.MinTimeToVisitAllPoints
+        (
+            @case.points
+        );
+
+        answer.Should().Be(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[][] points, int expected)> Examples()
+    {
+        yield return ([[1, 1], [3, 4], [-1, 0]], 7);
+        yield return ([[3, 2], [-2, 2]], 5);
+    }
+}
