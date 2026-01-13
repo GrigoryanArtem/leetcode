2f0ebf24fd5213717f53405e3543d906346c3c80
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-14 00:48:57 +0200

    3453

---
+++ Solutions/P3400/Problem3453.cs
@@ -0,0 +1,64 @@
+﻿namespace Solutions.P3400;
+
+public class Problem3453
+{
+    private const double EPS = 1e-6;
+
+    public double SeparateSquares(int[][] squares)
+    {
+        var area = Area(squares, out var minY, out var maxY);
+        var halfArea = area / 2;
+
+        while (Math.Abs(maxY - minY) > EPS)
+        {
+            var mid = (maxY + minY) / 2;
+
+            if (Area(squares, mid) >= halfArea)
+            {
+                maxY = mid;
+            }
+            else
+            {
+                minY = mid;
+            }
+        }
+
+        return Math.Round(maxY, 5);
+    }
+
+    private static double Area(int[][] squares, out double minY, out double maxY)
+    {
+        var area = .0;
+
+        maxY = Double.NegativeInfinity;
+        minY = Double.PositiveInfinity;
+
+        for (int i = 0; i < squares.Length; i++)
+        {
+            var y = squares[i][1];
+            var size = (double)squares[i][2];
+
+            area += size * size;
+
+            minY = Math.Min(minY, y);
+            maxY = Math.Max(maxY, y + size);
+        }
+
+        return area;
+    }
+
+    private static double Area(int[][] squares, double limitY)
+    {
+        var area = .0;
+
+        for (int i = 0; i < squares.Length; i++)
+        {
+            var y = squares[i][1];
+            var size = squares[i][2];
+
+            area += Math.Clamp(limitY - y, .0, size) * size;
+        }
+
+        return area;
+    }
+}
---
+++ Tests/P3400/Problem3453Tests.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.P3400;
+
+namespace Tests.P3400;
+
+[Trait("Problem", "3400+")]
+public class Problem3453Tests
+{
+
+    [Theory(DisplayName = "3453")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void TestExamples((int[][] squares, double expected) @case)
+    {
+        var problem = new Problem3453();
+
+        var answer = problem.SeparateSquares
+        (
+            @case.squares
+        );
+
+        answer.Should().Be(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[][] squares, double expected)> Examples()
+    {        
+        yield return ([[0, 0, 1], [2, 2, 1]], 1.0);
+        yield return ([[0, 0, 2], [1, 1, 1]], 1.16667);
+    }
+}
