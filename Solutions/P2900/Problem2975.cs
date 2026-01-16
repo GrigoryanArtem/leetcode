3d5314e45eab66a640cd8ca1da2d3b4c62e47b06
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-17 01:08:50 +0200

    2975

---
+++ Solutions/P2900/Problem2975.cs
@@ -0,0 +1,47 @@
+﻿namespace Solutions.P2900;
+
+public class Problem2975
+{
+    private const int MOD = 1000000007;
+
+    public int MaximizeSquareArea(int m, int n, int[] hFences, int[] vFences)
+    {
+        Span<int> h = stackalloc int[hFences.Length + 2];
+
+        h[0] = 1;
+        h[^1] = m;
+
+        Span<int> v = stackalloc int[vFences.Length + 2];
+
+        v[0] = 1;
+        v[^1] = n;
+
+        hFences.AsSpan().CopyTo(h.Slice(1, hFences.Length));
+        vFences.AsSpan().CopyTo(v.Slice(1, vFences.Length));
+
+        h.Sort();
+        v.Sort();
+
+        HashSet<int> sizes = [];
+
+        for (int i = 0; i < h.Length; i++)
+            for (int k = i + 1; k < h.Length; k++)
+                sizes.Add(h[k] - h[i]);
+
+        var max = -1;
+        for (int i = 0; i < v.Length; i++)
+        {
+            for (int k = i + 1; k < v.Length; k++)
+            {
+                var d = v[k] - v[i];
+
+                if (sizes.Contains(d))
+                    max = Math.Max(max, d);
+            }
+        }
+
+        return max > 0 
+            ? (int)(((long)max * max) % MOD) 
+            : -1; ;
+    }
+}
---
+++ Tests/P2975/Problem2975Tests.cs
@@ -0,0 +1,31 @@
+﻿using Solutions.P2900;
+
+namespace Tests.P2975;
+
+[Trait("Problem", "2900+")]
+public class Problem2975Tests
+{
+    [Theory(DisplayName = "2975")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void TestExamples((int m, int n, int[] hFences, int[] vFences, int expected) @case)
+    {
+        var problem = new Problem2975();
+
+        var answer = problem.MaximizeSquareArea
+        (
+            @case.m,
+            @case.n,
+            @case.hFences,
+            @case.vFences
+        );
+
+        answer.Should().Be(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int m, int n, int[] hFences, int[] vFences, int expected)> Examples()
+    {
+        yield return (4, 3, [2, 3], [2], 4);
+        yield return (6, 7, [2], [4], -1);
+    }
+}
