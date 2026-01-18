66630724e2b88b6ba1509245d686d543bf587da9
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-18 22:15:07 +0200

    1895

---
+++ Solutions/P1800/Problem1895.cs
@@ -0,0 +1,63 @@
+﻿namespace Solutions.P1800;
+
+public class Problem1895
+{
+    private int[,] hs;
+    private int[,] vs;
+    private int[,] ds;
+    private int[,] rds;
+
+    public int LargestMagicSquare(int[][] grid)
+    {
+        Init(grid);
+
+        var maxSize = Math.Min(grid.Length, grid[0].Length);
+        for (int size = maxSize; size > 1; size--)
+            for (int i = 0; i <= grid.Length - size; i++)
+                for (int k = 0; k <= grid[0].Length - size; k++)
+                    if (IsMatrixValid(i, k, size))
+                        return size;
+
+        return 1;
+    }
+
+    private void Init(int[][] grid)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        hs = new int[m, n + 1];
+        vs = new int[m + 1, n];
+        ds = new int[m + 1, n + 1];
+        rds = new int[m + 1, n + 1];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                hs[i, k + 1] = hs[i, k] + grid[i][k];
+                vs[i + 1, k] = vs[i, k] + grid[i][k];
+                ds[i + 1, k + 1] = ds[i, k] + grid[i][k];
+
+                var rk = n - 1 - k;
+                rds[i + 1, rk] = rds[i, rk + 1] + grid[i][rk];
+            }
+        }
+    }
+
+    public bool IsMatrixValid(int x, int y, int size)
+    {
+        var isValid = true;
+        var sum = hs[x, y + size] - hs[x, y];
+
+        for (int i = x + 1; isValid && i < x + size; i++)
+            isValid &= (hs[i, y + size] - hs[i, y]) == sum;
+
+        for (int i = y; isValid && i < y + size; i++)
+            isValid &= (vs[x + size, i] - vs[x, i]) == sum;
+
+        return isValid
+            && (ds[x + size, y + size] - ds[x, y] == sum)
+            && (rds[x + size, y] - rds[x, y + size] == sum);
+    }
+}
---
+++ Tests/P1800/Problem1895Tests.cs
@@ -0,0 +1,28 @@
+﻿using Solutions.P1800;
+
+namespace Tests.P1800;
+
+[Trait("Problem", "1800+")]
+public class Problem1895Tests
+{
+    [Theory(DisplayName = "1895")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void TestExamples((int[][] grid, int expected) @case)
+    {
+        var problem = new Problem1895();
+
+        var answer = problem.LargestMagicSquare
+        (
+            @case.grid
+        );
+
+        answer.Should().Be(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[][] grid, int expected)> Examples()
+    {
+        yield return ([[7, 1, 4, 5, 6], [2, 5, 1, 6, 4], [1, 5, 4, 3, 2], [1, 2, 7, 3, 4]], 3);        
+        yield return ([[5, 1, 3, 1], [9, 3, 3, 1], [1, 3, 3, 8]], 2);
+    }
+}
