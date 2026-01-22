def82803047a4c22e6479609b22ce52676744bfd
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-01-23 00:55:11 +0200

    3507

---
+++ Solutions/P3500/Problem3507.cs
@@ -0,0 +1,43 @@
+﻿namespace Solutions.P3500;
+
+public class Problem3507
+{
+    public int MinimumPairRemoval(int[] nums)
+    {
+        var data = new List<int>(nums);
+
+        var count = 0;
+        var sorted = false;
+        var minSum = 0;
+
+        while (!sorted && data.Count > 1)
+        {
+            sorted = true;
+
+            var minIndex = 0;
+            minSum = data[0] + data[1];
+
+            for (int i = 1; i < data.Count; i++)
+            {
+                var sum = data[i] + data[i - 1];
+
+                if (sum < minSum)
+                {
+                    minIndex = i - 1;
+                    minSum = sum;
+                }
+
+                sorted &= data[i] >= data[i - 1];
+            }
+
+            if (sorted)
+                break;
+
+            data[minIndex + 1] = minSum;
+            data.RemoveAt(minIndex);
+            count++;
+        }
+
+        return count;
+    }
+}
---
+++ Tests/P3500/Problem3507Tests.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.P3500;
+
+namespace Tests.P3500;
+
+[Trait("Problem", "3500+")]
+public class Problem3507Tests
+{
+    [Theory(DisplayName = "3507")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void TestExamples((int[] nums, int expected) @case)
+    {
+        var problem = new Problem3507();
+
+        var answer = problem.MinimumPairRemoval
+        (
+            @case.nums
+        );
+
+        answer.Should().Be(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[] nums, int expected)> Examples()
+    {
+        yield return ([-2, 1, 2, -1, -1, -2, -2, -1, -1, 1, 1], 9);
+        yield return ([5, 2, 3, 1], 2);
+        yield return ([1, 2, 2], 0);
+    }
+}
