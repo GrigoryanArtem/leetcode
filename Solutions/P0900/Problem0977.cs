f82df9ceabeb0305f07d4ea5f24eb1e7b6fa8024
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-04-03 14:23:13 +0300

    977

---
+++ Solutions/P0900/Problem0977.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace Solutions.P0900;
+
+public class Problem0977
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        var span = nums.AsSpan();
+        var tail = span.Length - (span.Length % Vector<int>.Count);
+
+        var vectors = MemoryMarshal.Cast<int, Vector<int>>(span);
+        for (int i = 0; i < vectors.Length; i++)
+            vectors[i] *= vectors[i];
+
+        for (int i = tail; i < span.Length; i++)
+            span[i] *= span[i]; 
+
+        var ans = new int[span.Length];
+        var aidx = span.Length;
+
+        var lo = 0;
+        var hi = span.Length - 1;
+
+        while (lo <= hi)
+            ans[--aidx] = span[lo] > span[hi] ? span[lo++] : span[hi--];
+
+        return ans;
+    }
+}
---
+++ Tests/P0900/Problem0977Tests.cs
@@ -0,0 +1,25 @@
+﻿using Solutions.P0900;
+
+namespace Tests.P0900;
+
+[Trait("Problem", "900+")]
+public class Problem0977Tests
+{
+
+    [Theory(DisplayName = "977")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void ExamplesTest((int[] nums, int[] expected) @case)
+    {
+        var problem = new Problem0977();
+
+        var answer = problem.SortedSquares(@case.nums);
+        answer.Should().BeEquivalentTo(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[] nums, int[] expected)> Examples()
+    {
+        yield return ([-4, -1, 0, 3, 10, 10, 10, 10, 10], [0, 1, 9, 16, 100, 100, 100, 100, 100]);
+        yield return ([-7, -3, 2, 3, 11], [4, 9, 9, 49, 121]);
+    }
+}
