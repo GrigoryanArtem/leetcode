ff217d843fb54c6f529e8764438f664c5b163e14
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-04-04 22:23:40 +0300

    2075

---
+++ Solutions/P2000/Problem2075.cs
@@ -0,0 +1,31 @@
+﻿namespace Solutions.P2000;
+
+public class Problem2075
+{
+    public string DecodeCiphertext(string encodedText, int rows)
+    {
+        if (encodedText.Length == 0)
+            return encodedText;
+
+        var cols = encodedText.Length / rows;
+        Span<char> ans = stackalloc char[(2 * cols - (rows - 1)) * rows / 2];
+
+        var aidx = 0;
+        var lst = 0;
+
+        for (int i = 0; i < cols; i++)
+        {
+            int maxK = Math.Min(rows, cols - i);
+            for (int k = 0; k < maxK; k++)
+            {
+                var ch = encodedText[k * cols + i + k];
+                ans[aidx++] = ch;
+
+                if (ch != ' ')
+                    lst = aidx;
+            }
+        }
+
+        return new string(ans[..lst]);
+    }
+}
---
+++ Tests/P2000/Problem2075Tests.cs
@@ -0,0 +1,26 @@
+﻿using Solutions.P2000;
+
+namespace Tests.P2000;
+
+[Trait("Problem", "2000+")]
+public class Problem2075Tests
+{
+
+    [Theory(DisplayName = "2075")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void ExamplesTest((string encodedText, int rows, string expected) @case)
+    {
+        var problem = new Problem2075();
+
+        var answer = problem.DecodeCiphertext(@case.encodedText, @case.rows);
+        answer.Should().Be(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(string encodedText, int rows, string expected)> Examples()
+    {
+        yield return ("iveo    eed   l te   olc", 4, "i love leetcode");
+        yield return ("ch   ie   pr", 3, "cipher");
+        yield return ("coding", 1, "coding");        
+    }
+}
