2e286cd7efeb8690abecb6af67acc4f02e8ef774
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-04-17 03:34:10 +0300

    3896

---
+++ Solutions/P3800/Problem3896.cs
@@ -0,0 +1,59 @@
+﻿namespace Solutions.P3800;
+
+public class Problem3896
+{
+    private const int N = 100100;
+
+    private static bool _init = false;
+    private static bool[] _primes = [];
+    private static int[] _next = [];
+
+    public int MinOperations(int[] nums)
+    {
+        RecalculatePrimes();
+
+        var permutations = 0;
+
+        for (int i = 1; i < nums.Length; i += 2)
+            if (_primes[nums[i]])
+                permutations += nums[i] == 2 ? 2 : 1;
+
+
+        for (int i = 0; i < nums.Length; i += 2)
+            if (!_primes[nums[i]])
+                permutations += _next[nums[i]] - nums[i];
+
+        return permutations;
+    }
+
+    private static void RecalculatePrimes()
+    {
+        if (_init)
+            return;
+
+        _init = true;
+        _primes = new bool[N];
+        _next = new int[N];
+
+        Array.Fill(_primes, true);
+        _primes[0] = _primes[1] = false;
+
+        for (int i = 2; i * i < N; i++)
+        {
+            if (!_primes[i])
+                continue;
+
+            for (int j = i * i; j < N; j += i)
+                _primes[j] = false;
+        }
+
+        var current = 0;
+        for (int i = N - 1; i >= 0; i--)
+        {
+            if (_primes[i])
+                current = i;
+
+            _next[i] = current;
+        }
+    }
+}
---
+++ Tests/P3800/Problem3896Tests.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.P3800;
+
+namespace Tests.P3800;
+
+[Trait("Problem", "3800+")]
+public class Problem3896Tests
+{
+    [Theory(DisplayName = "3896")]
+    [MemberData(nameof(ExamplesMemberData))]
+    public void ExamplesTest((int[] nums, int expected) @case)
+    {
+        var problem = new Problem3896();
+
+        var answer = problem.MinOperations(@case.nums);
+        answer.Should().Be(@case.expected);
+
+        var answer2 = problem.MinOperations(@case.nums);
+        answer2.Should().Be(@case.expected);
+    }
+
+    public static IEnumerable<object?[]> ExamplesMemberData => Examples().ToMemberData();
+    public static IEnumerable<(int[] nums, int expected)> Examples()
+    {
+        yield return ([1, 2, 3, 4], 3);
+        yield return ([5, 6, 7, 8], 0);
+        yield return ([4, 4], 1);        
+        yield return ([11115, 4776, 2638, 7066, 11689, 13033, 8698, 11252, 11168, 5376, 11670, 6619, 1557, 1721, 8288, 9763, 12242, 3854, 4723, 659, 2176, 1205, 10631, 3266, 435, 4694, 10580, 3920, 5400, 11779, 4970, 6081, 4637, 3317, 11251, 5324, 2246, 9212, 13077, 8148, 7008, 12055, 5286, 9951, 10202, 6727, 3480, 9186, 1269, 2679, 4235, 12309, 13242, 12283, 2283, 11765, 4527, 6125, 4972, 6422, 729, 5904, 3276, 9142, 10221, 11109, 11261, 9386, 11227, 6661, 3836, 13222, 870, 8, 5179, 7999, 3890, 13010, 6996, 1125, 942, 53, 4199, 5063, 5599, 4118, 11931, 10524, 3385, 2938, 8729, 12389, 11272, 11542, 9856, 12168, 12128, 2799, 12531, 7268, 9462, 13381, 11084, 7257, 7724, 10388, 6050, 10113, 11310, 4427, 6823, 13219, 8492, 10689, 11577, 3241, 5064], 350);        
+    }
+}
